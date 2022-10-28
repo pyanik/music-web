1d2f9271ee932099cbc8990159f70fe0af0fa13c
pyanik <pyanik@users.noreply.github.com>
2022-10-28 23:23:57 +0200

    add main classes

---
+++ src/main/java/com/pyanik/musicweb/MusicWebApplication.java
@@ -0,0 +1,13 @@
+package com.pyanik.musicweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pyanik/musicweb/MusicWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyanik.musicweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
